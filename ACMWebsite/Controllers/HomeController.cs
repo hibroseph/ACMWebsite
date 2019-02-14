3db03f172e32760246a6cdffe5a4004eff5370df
hibroseph <iwillridgley@gmail.com>
2019-02-13 22:15:35 -0800

    Added leadership page

--- ACMWebsite/Controllers/HomeController.cs
+++ ACMWebsite/Controllers/HomeController.cs
@@ -20,6 +20,10 @@ namespace ACMWebsite.Controllers
             return View();
         }
         
+        public IActionResult Leadership()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
